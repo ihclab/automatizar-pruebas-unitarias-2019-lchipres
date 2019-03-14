ead32d9ded11cd53e8f1a5a21f2e3ca985b0bac6
Leonardo Daniel Chipres Arteaga <33239864+lchipres@users.noreply.github.com>
2019-03-14 10:23:28 -0600

    Problema 00

--- Medias.cs
+++ Medias.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.math;
 
 namespace AutomatizarPruebasUnitarias {
     
@@ -11,21 +12,70 @@ namespace AutomatizarPruebasUnitarias {
         /**
          * Calcula y regresa la media artimética
          */
-        public static double mediaAritmetica(params int[] vals) { }
-
+        public static double mediaAritmetica(params int[] vals) 
+        {
+        double sum=0;
+        
+        sum=sumaArray(vals)/vals.Length;
+        
+        return sum;
+        }
+        
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
          */
-        private static double raizEnesima(double x, int n) { }
-
+        private static double raizEnesima(double x, int n) 
+        {
+            double raiz=0;
+            double y=n;
+            raiz= Math.Pow(x,1/y);
+            
+            return raiz;
+        }
+        
         /**
          *  Usa raizEnesima para calcular y regresar la media geométrica
          */
-        public double mediaGeometrica(params int[] vals) { }
+        public double mediaGeometrica(params int[] vals) 
+        {
+            double media;
+            media=multArray();
+            media=raizEnesima(media,vals.Length);
+            return media;
+        }
 
         /**
          * Este método no está implementado.
          */
-        public static double mediaArmonica(params int[] vals) { }
+        public static double mediaArmonica(params int[] vals) 
+        {
+			double media=0;
+			
+			return media;
+        }
+
+        public static double sumaArray(params int[] vals)
+        {
+            double sum=0;
+
+            for (int i = 0; i < vals.Length; i++)
+            {
+                sum+=vals[i];
+            }
+
+            return sum;
+        }
+
+        public static double multArray(params int[] vals)
+        {
+            double mult=1;
+
+            for (int i = 0; i < vals.Length ; i++)
+            {
+                mult*=vals[i];
+            }
+
+            return mult;
+        }
     }
 }
